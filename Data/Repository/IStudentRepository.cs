5df1694b61975ca67db0da78ff73197bdbf429a1
sarathraj <srajqtr@gmail.com>
2024-02-02 21:30:11 +0300

    table specif functionality

--- Data/Repository/IStudentRepository.cs
+++ Data/Repository/IStudentRepository.cs
@@ -3,14 +3,10 @@ using System;
 
 namespace school.Data.Repository
 {
-    public interface IStudentRepository
+    public interface IStudentRepository : ICollegeRepository<Student>
     {
-      public  Task<List<Student>> GetAllStudents();
-       public   Task<Student> GetStudentById(int id);
-       public   Task<Student> GetStudentByName(string name);
-        public   Task<int> CreateStudent(Student student);
-       public Task<int> UpdateStudent(Student student);
-       public Task<bool> DeleteStudent(int id);
+    
+     List<Student> GetAllStudentsByFeesStatus( int feesStatus);
 
     }
 }
--- Data/Repository/StudentRepository.cs
+++ Data/Repository/StudentRepository.cs
@@ -3,71 +3,77 @@ using Microsoft.EntityFrameworkCore;
 
 namespace school.Data.Repository
 {
-    public class StudentRepository : IStudentRepository
+    public class StudentRepository : CollegeRepository<Student>, IStudentRepository
     {
         private readonly CollegeDBContext _collegeDBContext;
 
         
-        public StudentRepository(CollegeDBContext collegeDBContext)
+        public StudentRepository(CollegeDBContext collegeDBContext) : base(collegeDBContext)
         {
             _collegeDBContext = collegeDBContext;
         }
-        public async Task<int> CreateStudent(Student student)
-        {
-            _collegeDBContext.Students.Add(student);
-            await _collegeDBContext.SaveChangesAsync();
-            return student.Id;
-        }
 
-        public async Task<bool> DeleteStudent(int id)
+        // Table specific methods
+        public List<Student> GetAllStudentsByFeesStatus(int feesStatus)
         {
-            var studentToUpdate = await _collegeDBContext.Students.Where(i => i.Id == id).FirstOrDefaultAsync();
-            if (studentToUpdate == null)
-            {
-                throw new Exception("Student not found");
-            }
-            if (studentToUpdate != null)
-            {
-                _collegeDBContext.Students.Remove(studentToUpdate);
-                _collegeDBContext.SaveChanges();
-            }
-            return true;
-
+            throw new NotImplementedException();
         }
+        // public async Task<int> CreateStudent(Student student)
+        // {
+        //     _collegeDBContext.Students.Add(student);
+        //     await _collegeDBContext.SaveChangesAsync();
+        //     return student.Id;
+        // }
 
-        public async Task<List<Student>> GetAllStudents()
-        {
-            return await _collegeDBContext.Students.ToListAsync();
-        }
+        // public async Task<bool> DeleteStudent(int id)
+        // {
+        //     var studentToUpdate = await _collegeDBContext.Students.Where(i => i.Id == id).FirstOrDefaultAsync();
+        //     if (studentToUpdate == null)
+        //     {
+        //         throw new Exception("Student not found");
+        //     }
+        //     if (studentToUpdate != null)
+        //     {
+        //         _collegeDBContext.Students.Remove(studentToUpdate);
+        //         _collegeDBContext.SaveChanges();
+        //     }
+        //     return true;
 
-        public async Task<Student> GetStudentById(int id)
-        {
-            var student = await _collegeDBContext.Students.Where(i => i.Id == id).FirstOrDefaultAsync();
-            return student;
-        }
+        // }
 
-        public async Task<Student> GetStudentByName(string name)
-        {
-            var student = await _collegeDBContext.Students.Where(i => i.Name == name).FirstOrDefaultAsync();
-            return student;
-        }
+        // public async Task<List<Student>> GetAllStudents()
+        // {
+        //     return await _collegeDBContext.Students.ToListAsync();
+        // }
 
-        public async Task<int> UpdateStudent(Student student)
-        {
-            var studentToUpdate = await _collegeDBContext.Students.Where(i => i.Id == student.Id).FirstOrDefaultAsync();
-            if (studentToUpdate == null)
-            {
-                throw new Exception("Student not found");
-            }
-            if (studentToUpdate != null)
-            {
-                studentToUpdate.Name = student.Name;
-                studentToUpdate.Address = student.Address;
-                studentToUpdate.Email = student.Email;
-                studentToUpdate.Dob = student.Dob;
-                await _collegeDBContext.SaveChangesAsync();
-            }
-            return studentToUpdate.Id;
-        }
+        // public async Task<Student> GetStudentById(int id)
+        // {
+        //     var student = await _collegeDBContext.Students.Where(i => i.Id == id).FirstOrDefaultAsync();
+        //     return student;
+        // }
+
+        // public async Task<Student> GetStudentByName(string name)
+        // {
+        //     var student = await _collegeDBContext.Students.Where(i => i.Name == name).FirstOrDefaultAsync();
+        //     return student;
+        // }
+
+        // public async Task<int> UpdateStudent(Student student)
+        // {
+        //     var studentToUpdate = await _collegeDBContext.Students.Where(i => i.Id == student.Id).FirstOrDefaultAsync();
+        //     if (studentToUpdate == null)
+        //     {
+        //         throw new Exception("Student not found");
+        //     }
+        //     if (studentToUpdate != null)
+        //     {
+        //         studentToUpdate.Name = student.Name;
+        //         studentToUpdate.Address = student.Address;
+        //         studentToUpdate.Email = student.Email;
+        //         studentToUpdate.Dob = student.Dob;
+        //         await _collegeDBContext.SaveChangesAsync();
+        //     }
+        //     return studentToUpdate.Id;
+        // }
     }
 }
